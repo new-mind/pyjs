4c46e22a9cf6bc09a4b22dc17d633e172a9f0413
Guro Bokum <jiojiajiu@gmail.com>
2015-04-02 22:45:18 +0700

    Runtime.cpp skelet

---
+++ src/Runtime.cpp
@@ -0,0 +1,12 @@
+#include <Python.h>
+
+PyObject *
+pyjs_runtime(PyObject *self, PyObject *args)
+{
+    PyObject *a;
+    return a;
+}
+
+static PyMethodDef methods[] {
+    {"Runtime", pyjs_runtime, 0, NULL},
+};
