30d7c6dacd1b2d9135ac34780fb79e7510d1ab4b
Guro Bokum <jiojiajiu@gmail.com>
2015-04-02 22:45:05 +0700

    compiling

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
-from setuptools import setup
+from setuptools import setup, Extension
+
+ext = Extension('pyjs', sources = ['src/Runtime.cpp'])
 
 setup(name='pyjs',
       version='0.0.1',
       autho='jiojiajiu',
-      email='jiojiajiu@gmail.com')
+      email='jiojiajiu@gmail.com',
+      ext_modules=[ext,])
