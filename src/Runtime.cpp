f6877b50459afbee727dffa74b178e33e556be49
Guro Bokum <jiojiajiu@gmail.com>
2015-04-03 21:28:03 +0700

    initpyjs

--- src/Runtime.cpp
+++ src/Runtime.cpp
@@ -10,3 +10,9 @@ pyjs_runtime(PyObject *self, PyObject *args)
 static PyMethodDef methods[] {
     {"Runtime", pyjs_runtime, 0, NULL},
 };
+
+PyMODINIT_FUNC
+initpyjs(void)
+{
+    (void) Py_InitModule("pyjs", methods);
+}
