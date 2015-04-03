8d3a27caba8ab867de081904e6f7f9ddfefc4b9a
Guro Bokum <jiojiajiu@gmail.com>
2015-04-03 22:15:04 +0700

    include_dirs, lib_dirs

--- setup.py
+++ setup.py
@@ -1,6 +1,13 @@
+import os
 from setuptools import setup, Extension
 
-ext = Extension('pyjs', sources = ['src/Runtime.cpp'])
+MOZJS = 'src/js/js/src'
+INCLUDE_DIRS = os.environ.get('INCLUDE_MOZJS', MOZJS)
+LIBRARY_DIRS = os.environ.get('LIBRARY_MOZJS', MOZJS)
+
+ext = Extension('pyjs', sources=['src/Runtime.cpp'],
+                        include_dirs=[INCLUDE_DIRS,],
+                        library_dirs=[LIBRARY_DIRS,],)
 
 setup(name='pyjs',
       version='0.0.1',
